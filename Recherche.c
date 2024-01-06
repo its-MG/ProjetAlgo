62c6221d63fc02b7cfa1d3628ac75a26a4de1a49
its-MG <128987407+its-MG@users.noreply.github.com>
2024-01-06 13:16:49 +0100

    Création de l'arbre et initialisation de la fenêtre

---
+++ Recherche.c
@@ -0,0 +1,136 @@
+#include "raylib.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CIRCLE_RADIUS 38
+#define HORIZONTAL_SPACE 250
+#define VERTICAL_SPACE 150
+#define MAX_INPUT_LENGTH 2 // l'utilisateur peut entrer un nombre a deux chiffres max
+
+char inputBuffer[MAX_INPUT_LENGTH + 1]; // pour l'operation de lecture au clavier
+int inputLength = 0;
+
+typedef struct TreeNode {
+    int data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+} TreeNode;
+
+TreeNode *createNode(int data) {
+    TreeNode *newNode = (TreeNode *)malloc(sizeof(TreeNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+bool searchNode(TreeNode *root, int value) {
+     bool foundLeft=false;
+     bool foundRight =false;
+
+    if (root == NULL) return false;
+    if (root->data == value) return true;
+
+    if(value<root->data){
+        foundLeft = searchNode(root->left, value); // Rechercher dans le sous-arbre gauche
+    }else{
+        foundRight = searchNode(root->right, value);// Rechercher dans le sous-arbre droit 
+    }
+
+    return(foundLeft || foundRight);
+}
+
+void insertNode(TreeNode *root, int index) {
+    if (root == NULL) return;
+
+    if (index > root->data) {
+        if (root->right == NULL) {
+            root->right = createNode(index);
+        } else {
+            insertNode(root->right, index);
+            DrawText("Enter a number :", 20, 20, 20, BLACK);
+        }
+    } else {
+        if (root->left == NULL) {
+            root->left = createNode(index);
+        } else {
+            insertNode(root->left, index);
+        }
+    }
+}
+
+void drawTree(TreeNode *root, int x, int y, int hSpacing, int vSpacing, int searchValue, bool *foundValue) {
+    if (root != NULL) {
+        int posX = x;
+        int posY = y;
+
+        if (root->left != NULL) {
+            DrawLine(posX, posY, posX - hSpacing, posY + vSpacing, (Color){ 0x98, 0xC1, 0xD9, 255 });
+            drawTree(root->left, posX - hSpacing, posY + vSpacing, hSpacing / 2, vSpacing, searchValue, foundValue);
+        }
+
+        if (root->right != NULL) {
+        DrawLine(posX, posY, posX + hSpacing, posY + vSpacing,(Color){ 0x98, 0xC1, 0xD9, 255 });
+        drawTree(root->right, posX + hSpacing, posY + vSpacing, hSpacing / 2, vSpacing, searchValue, foundValue);
+        }
+
+        bool currentNodeFound = (root->data == searchValue);
+
+        if (currentNodeFound) {
+            *foundValue = true;
+          Color foundColor = (Color){100, 180, 100, 255}; // couleur vert si la valeur est trouvée
+            DrawCircle(posX, posY, CIRCLE_RADIUS, foundColor);
+        } else {
+            Color customColor ={ 0x1F, 0x5E, 0x91, 255 }; // couleur bleu pour les autres noeuds
+            DrawCircle(posX, posY, CIRCLE_RADIUS, customColor);
+        }
+
+        int textWidth = MeasureText(TextFormat("%d", root->data), 20);
+        int textHeight = 20;
+        int textX = posX - textWidth / 2;
+        int textY = posY - textHeight / 2;
+        DrawText(TextFormat("%d", root->data), textX, textY, 20,WHITE);
+    }
+}
+
+int main() {
+
+    const int screenWidth = 1000;
+    const int screenHeight = 800;
+
+    InitWindow(screenWidth, screenHeight, "Recherche dans un arbre Binaire");
+
+    SetTargetFPS(60);
+
+     // ces numéros ne sont pas choisis aléatoirement c'est pour crée un arbre binaire équlibré
+     TreeNode *root = createNode(16); // la racine
+     insertNode(root,8);
+     insertNode(root,24);
+     insertNode(root,4);
+     insertNode(root,12);
+     insertNode(root,20);
+     insertNode(root,28);
+     insertNode(root,18);
+     insertNode(root,22);
+     insertNode(root,26);
+     insertNode(root,30);
+     insertNode(root,2);
+     insertNode(root,6);
+     insertNode(root,10);
+     insertNode(root,14);
+
+    Color backgroundColor = {0x0A, 0x23, 0x44, 255 };  // couleur arrière-plan
+
+    while (!WindowShouldClose()) {
+
+    BeginDrawing();
+    ClearBackground(backgroundColor);
+    EndDrawing();
+
+}
+    CloseWindow();
+    return 0;
+}
